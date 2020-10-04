9f75ee3a5370c96795935039d00fe00b0a50585e
RS0099 <53785276+RS0099@users.noreply.github.com>
2020-10-04 15:33:30 +0530

    Week -2 - Python script
    Added the script used to count the frequency of unique word_pos and store them in a csv

---
+++ week2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import csv
+
+
+word_tag_dict={}
+
+with open('word_tag.csv','r') as file:
+	reader=csv.DictReader(file)
+	for row in reader:
+		word_tag=row["Word_pos"]
+		if word_tag not in word_tag_dict.keys():
+			word_tag_dict[word_tag]=0
+
+		word_tag_dict[word_tag]=word_tag_dict[word_tag] + 1	
+
+
+with open('word_tag_freq.csv','w') as file:
+	writer =csv.writer(file)
+	writer.writerow(['Word_tag','Frequency'])
+	for word,freq in word_tag_dict.items():
+		writer.writerow([word,freq])
+
