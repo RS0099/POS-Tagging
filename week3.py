6aafb9e9c2d01c806cf4bc7bef651a9321ac7b71
Anmol Agarwal <agarwalanmol922@gmail.com>
2020-10-05 16:19:11 +0530

    Week - 3 - Python Script

--- week3.py
+++ week3.py
@@ -12,9 +12,11 @@ csv_file_test = open(path_test, encoding="utf8")
 
 for row in csv.reader(csv_file_train, delimiter=','):
     if row[1] != 'Frequency': 
-        if row[0] not in word_freq_map_train:
-            word_freq_map_train[row[0]] = int(row[1])
-        word_freq_map_train[row[0]] = word_freq_map_train[row[0]] + int(row[1])
+        word = row[0].split("_")[0]
+        if word not in word_freq_map_train:
+            word_freq_map_train[word] = int(row[1])
+        word_freq_map_train[word] = word_freq_map_train[word] + int(row[1])
+
 
 print("\n \nTop 10 frequent used words in train set")
 for x, y in Counter(word_freq_map_train).most_common(10):
@@ -22,9 +24,10 @@ for x, y in Counter(word_freq_map_train).most_common(10):
 
 for row in csv.reader(csv_file_test, delimiter=','):
     if row[1] != 'Frequency': 
-        if row[0] not in word_freq_map_test:
-            word_freq_map_test[row[0]] = int(row[1])
-        word_freq_map_test[row[0]] = word_freq_map_test[row[0]] + int(row[1])
+        word = row[0].split("_")[0]
+        if word not in word_freq_map_test:
+            word_freq_map_test[word] = int(row[1])
+        word_freq_map_test[word] = word_freq_map_test[word] + int(row[1])
 
 print("\n \nTop 10 frequent used words in test set")
 for x, y in Counter(word_freq_map_test).most_common(10):
