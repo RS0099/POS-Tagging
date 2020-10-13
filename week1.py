743a524a8e203adb15b8689fe904be2592f9dfb2
adityakrsingh007 <53978588+adityakrsingh007@users.noreply.github.com>
2020-10-13 18:50:50 +0530

    Update week1.py

--- week1.py
+++ week1.py
@@ -1,35 +1,46 @@
-import xml.dom.minidom as xd
 import os
 import csv
-from xml.dom.minidom import parse
+import xml.etree.ElementTree as ET 	
 from fnmatch import fnmatch
 
-root='/home/rs/AI_project/Assignment-files/Train-corups/'
+root='/home/aditya/Downloads/Train-corups/'
 pattern="*.xml"
 
 all_file_paths=list()
-row_list=[["S.No","Word_pos"]]
-n=1
 
 folders=os.listdir(path=root)
 for folder in folders:
 	f_path=os.path.join(root,folder)
+	
 	files=os.listdir(path=f_path)
 	for file in files:
 		f2_path=os.path.join(f_path,file)
 		all_file_paths.append(f2_path)
 
+string = ""
 
 for file_path in all_file_paths:
-	DOMTREE =xd.parse(file_path)
-	w_tags=DOMTREE.getElementsByTagName("w")
-	for w in w_tags:
-		word=w.getAttribute("hw")
-		tag=w.getAttribute("pos")
-		row=[str(n),word+"_"+tag]
-		row_list.append(row)
-		n=n+1
+	tree = ET.parse(file_path)
+	root = tree.getroot()
+	for c in root.findall('wtext'):
+		for d in c.findall('div'):
+			for e in d:
+				for f in e.findall('s'):
+					for g in f:
+						tag = g.tag
+						if tag == 'w':
+							string = " " + g.attrib.get('hw')+"_"+g.attrib.get('pos')
+							with open('word_tag_test.txt','a')as file:
+								file.write(string)
+						elif tag == 'c':
+							string = " " + g.text.replace(" ", "")
+							with open('word_tag_test.txt','a')as file:
+								file.write(string)
+				if e.tag == 'head':
+					string = '\n'
+					with open('word_tag_test.txt','a')as file:
+								file.write(string)
 
-with open('word_tag.csv','w')as file:
-	writer=csv.writer(file)
-	writer.writerows(row_list)
+	string = '\n'
+	with open('word_tag_test.txt','a')as file:
+				file.write(string)
